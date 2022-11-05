038502f234e72805675a929c43dde0fa7eef469c
Zhangmj0621 <2270170683@qq.com>
2022-11-06 00:18:22 +0800

    run commit

--- eqminer/widget.cpp
+++ eqminer/widget.cpp
@@ -8,6 +8,7 @@
 #include"randomio.h"
 #include<iostream>
 #include<fstream>
+#include<stdlib.h>
 using namespace std;
 
 Widget::Widget(QWidget *parent)
@@ -41,59 +42,75 @@ Widget::Widget(QWidget *parent)
                 QString str=config.readnamelist().at(i);
                 qDebug()<<str;
             }
-            //获得输入input.txt
+            int maxnum=config.readnamelist().size();
+            bool judge[maxnum][maxnum]; //用来暂存是否判断等价
+            for(int i=0;i<maxnum;i++)
+                for(int j=0;j<maxnum;j++)
+                    judge[i][j]=true;   //初始设定为等价，后续只要判断出现一次不等价就不必在进行接下去的判断
+            //获得输入input.txt，进行十次判断
             Randomio randomio;
             fstream io_file(dir_path.toStdString()+"/input.txt",ios::in | ios::out);
-            for(int i=0;i<config.readinputBase().size();i++)
+            for(int k=0;k<9;k++)
             {
-                QString base=config.readinputBase().at(i);
-                if(base[0]=='i')
+                for(int i=0;i<config.readinputBase().size();i++)
                 {
-                    int a=0;
-                    int i=4;
-                    for(;i<base.size();i++)
+                    QString base=config.readinputBase().at(i);
+                    if(base[0]=='i')
                     {
-                        if(base[i]!=',')
+                        int a=0;
+                        int i=4;
+                        for(;i<base.size();i++)
                         {
-                            a=a*10+(base.toStdString()[i]-'0');
+                            if(base[i]!=',')
+                            {
+                                a=a*10+(base.toStdString()[i]-'0');
+                            }
+                            else break;
                         }
-                        else break;
-                    }
-                    int b=0;
-                    for(i=i+1;i<base.size()-1;i++)
-                    {
-                        b=b*10+base.toStdString()[i]-'0';
-                    }
-                    int an=randomio.getrandomint(a,b);
-                    io_file<<an;
-                }
-                else if(base[0]=='c')
-                {
-                    char an=randomio.getrandomchar();
-                    io_file<<an;
-                }
-                else
-                {
-                    int a=0;
-                    int i=7;
-                    for(;i<base.size();i++)
-                    {
-                        if(base[i]!=',')
+                        int b=0;
+                        for(i=i+1;i<base.size()-1;i++)
                         {
-                            a=a*10+(base.toStdString()[i]-'0');
+                            b=b*10+base.toStdString()[i]-'0';
                         }
-                        else break;
+                        int an=randomio.getrandomint(a,b);
+                        io_file<<an;
                     }
-                    int b=0;
-                    for(i=i+1;i<base.size()-1;i++)
+                    else if(base[0]=='c')
                     {
-                        b=b*10+base.toStdString()[i]-'0';
+                        char an=randomio.getrandomchar();
+                        io_file<<an;
                     }
-                    string an=randomio.getrandomstring(a,b);
-                    io_file<<an;
+                    else
+                    {
+                        int a=0;
+                        int i=7;
+                        for(;i<base.size();i++)
+                        {
+                            if(base[i]!=',')
+                            {
+                                a=a*10+(base.toStdString()[i]-'0');
+                            }
+                            else break;
+                        }
+                        int b=0;
+                        for(i=i+1;i<base.size()-1;i++)
+                        {
+                            b=b*10+base.toStdString()[i]-'0';
+                        }
+                        string an=randomio.getrandomstring(a,b);
+                        io_file<<an;
+                    }
+                    if(i!=config.readinputBase().size()-1) io_file<<" ";
                 }
-                if(i!=config.readinputBase().size()-1) io_file<<" ";
+                //选取两个文件运行得到结果
+                for(int i=0;i<maxnum;i++)
+                {
+                    string temp="gcc "+dir_path.toStdString()+"/"+config.readnamelist().at(i).toStdString();
+                    system(temp.c_str());
+                }
             }
+
+
         }
     });
 }
